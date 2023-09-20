bc49c93b3ac7d66fff5f50f0e7f82bdf7a5999df
Aoran Zeng <ccmywish@qq.com>
2023-09-20 21:06:02 +0800

    Add tools

---
+++ tools/download.ps1
@@ -0,0 +1,5 @@
+tmp
+curl -LO https://github.com/RubyMetric/chsrc/releases/download/preview/chsrc-x64-windows.exe
+curl -LO https://github.com/RubyMetric/chsrc/releases/download/preview/chsrc-x86-windows.exe
+curl -LO https://github.com/RubyMetric/chsrc/releases/download/preview/chsrc-x64-linux
+curl -LO https://github.com/RubyMetric/chsrc/releases/download/preview/chsrc-x64-macos
