5b5b13f4618100d3bae48e448bab1f995c1bf24b
Aoran Zeng <ccmywish@qq.com>
2023-09-21 21:35:01 +0800

    Update tools

--- tools/download.ps1
+++ tools/download.ps1
@@ -1,5 +1,8 @@
 tmp
 curl -LO https://github.com/RubyMetric/chsrc/releases/download/preview/chsrc-x64-windows.exe
 curl -LO https://github.com/RubyMetric/chsrc/releases/download/preview/chsrc-x86-windows.exe
-curl -LO https://github.com/RubyMetric/chsrc/releases/download/preview/chsrc-x64-linux
 curl -LO https://github.com/RubyMetric/chsrc/releases/download/preview/chsrc-x64-macos
+curl -LO https://github.com/RubyMetric/chsrc/releases/download/preview/chsrc-x64-linux
+curl -LO https://github.com/RubyMetric/chsrc/releases/download/preview/chsrc-aarch64-linux
+curl -LO https://github.com/RubyMetric/chsrc/releases/download/preview/chsrc-riscv64-linux
+curl -LO https://github.com/RubyMetric/chsrc/releases/download/preview/chsrc-armv7-linux
