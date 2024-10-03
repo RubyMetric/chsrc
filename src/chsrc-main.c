53820935dd60084d485119bdccaaaa23133a825c
Aoran Zeng <ccmywish@qq.com>
2024-10-04 03:08:54 +0800

    RC release `v0.1.9-rc1`

--- src/chsrc-main.c
+++ src/chsrc-main.c
@@ -14,13 +14,13 @@
  *                 |   Lontten     <lontten@163.com>
  *                 |
  * Created On      : <2023-08-28>
- * Last Modified   : <2024-10-03>
+ * Last Modified   : <2024-10-04>
  *
  * chsrc: Change Source —— 全平台通用命令行换源工具
  * ------------------------------------------------------------*/
 
-#define Chsrc_Version        "0.1.9.Beta4"
-#define Chsrc_Release_Date   "2024/10/03"
+#define Chsrc_Version        "0.1.9-rc1"
+#define Chsrc_Release_Date   "2024/10/04"
 #define Chsrc_Banner_Version "v" Chsrc_Version "-" Chsrc_Release_Date
 #define Chsrc_Maintain_URL   "https://github.com/RubyMetric/chsrc"
 #define Chsrc_Maintain_URL2  "https://gitee.com/RubyMetric/chsrc"
