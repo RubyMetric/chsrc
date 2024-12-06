18274d8e883b0c52a822c1701af014e66b27f847
Aoran Zeng <ccmywish@qq.com>
2024-12-06 11:08:46 +0800

    Prerelease `v0.1.9.5`

--- src/chsrc-main.c
+++ src/chsrc-main.c
@@ -25,8 +25,8 @@
  * chsrc: Change Source —— 全平台通用命令行换源工具
  * ------------------------------------------------------------*/
 
-#define Chsrc_Version        "0.1.9.4-dev1"
-#define Chsrc_Release_Date   "2024/11/22"
+#define Chsrc_Version        "0.1.9.5"
+#define Chsrc_Release_Date   "2024/12/06"
 #define Chsrc_Banner_Version "v" Chsrc_Version "-" Chsrc_Release_Date
 #define Chsrc_Maintain_URL   "https://github.com/RubyMetric/chsrc"
 #define Chsrc_Maintain_URL2  "https://gitee.com/RubyMetric/chsrc"
