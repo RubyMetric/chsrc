5a8a75eb7178d06c7e49609412319ce6278cde25
Aoran Zeng <ccmywish@qq.com>
2024-12-14 19:52:08 +0800

    Frameworker test more

--- test/fw.c
+++ test/fw.c
@@ -8,13 +8,19 @@
  * Last Modified : <2024-12-14>
  * ------------------------------------------------------------*/
 
-#define Chsrc_Version "sid"
+#define Chsrc_Version "Frameworker"
 
 #include "../src/framework/core.c"
 
 int
 main (int argc, char const *argv[])
 {
+  if (argc == 2 && xy_streql (argv[1], "--version"))
+    {
+      say (Chsrc_Version);
+      return 0;
+    }
+
   br();
   chsrc_log  ("chsrc_log");
   chsrc_succ ("chsrc_succ");
@@ -25,14 +31,36 @@ main (int argc, char const *argv[])
   chsrc_verbose ("chsrc_verbose");
 
   chsrc_succ2 ("chsrc_succ2");
-  chsrc_log2 ("chsrc_log2");
+  chsrc_log2  ("chsrc_log2");
   chsrc_note2 ("chsrc_note2");
   chsrc_warn2 ("chsrc_warn2");
   chsrc_error2 ("chsrc_error2");
   chsrc_debug2 ("chsrc_debug2");
   chsrc_verbose2 ("chsrc_verbose2");
 
+
+  assert (chsrc_check_program ("./fw"));
+  assert (chsrc_check_program_quietly ("./fw"));
+  assert (chsrc_check_program_quietly_when_exist ("./fw"));
+  chsrc_ensure_program ("./fw");
+
+  #define bkup "README.md.bak"
+
   chsrc_backup ("README.md");
-  chsrc_succ2 ("Test framework successfully!");
+  assert (chsrc_check_file (bkup));
+
+  chsrc_ensure_dir ("test");
+
+  chsrc_append_to_file  ("append",  bkup);
+  chsrc_prepend_to_file ("prepend", bkup);
+  chsrc_overwrite_file  ("overwrite", bkup);
+  chsrc_view_file (bkup);
+
+  chsrc_log (xy_2strjoin ("CPU arch = ", chsrc_get_cpuarch ()));
+  printf ("chsrc: CPU cores = ");
+  puti (chsrc_get_cpucore ());
+
+  chsrc_run ("rm " bkup, RunOpt_No_Last_New_Line);
+  chsrc_run ("echo " Chsrc_Version " test pass!", RunOpt_Dont_Notify_On_Success);
   return 0;
 }
