09fba2b8884214189c8ea4a24a034353a7ec3cd1
Aoran Zeng <ccmywish@qq.com>
2025-04-13 18:41:55 +0800

    Advance to release `v0.2.1` We don't know if we can still rely on GitHub in the future

--- src/chsrc-main.c
+++ src/chsrc-main.c
@@ -26,13 +26,13 @@
  *                 |     ccy        <icuichengyi@gmail.com>
  *                 |
  * Created On      : <2023-08-28>
- * Last Modified   : <2025-03-25>
+ * Last Modified   : <2025-04-13>
  *
  * chsrc: Change Source —— 全平台通用命令行换源工具
  * ------------------------------------------------------------*/
 
-#define Chsrc_Version        "0.2.0.1"
-#define Chsrc_Release_Date   "2025/03/17"
+#define Chsrc_Version        "0.2.1"
+#define Chsrc_Release_Date   "2025/04/13"
 #define Chsrc_Maintain_URL   "https://github.com/RubyMetric/chsrc"
 #define Chsrc_Maintain_URL2  "https://gitee.com/RubyMetric/chsrc"
 
