4dc7908e747f3423c5ac434614764d63d9e59621
Aoran Zeng <ccmywish@qq.com>
2025-07-13 20:36:23 +0800

    Update download script

--- tool/download-pre-on-GitHub.ps1
+++ tool/download-pre-on-GitHub.ps1
@@ -2,7 +2,7 @@
 # File Name     : download-pre-on-GitHub.ps1
 # File Authors  : Aoran Zeng <ccmywish@qq.com>
 # Created On    : <2023-09-21>
-# Last Modified : <2025-06-16>
+# Last Modified : <2025-07-13>
 #
 # Download all files from the `pre` release on GitHub:
 #   https://github.com/RubyMetric/chsrc/releases/tag/pre
@@ -29,9 +29,20 @@ $names = @(
 # Like https://github.com/RubyMetric/chsrc/releases/download/latest/chsrc-x64-windows.exe
 $url_prefix = "https://github.com/RubyMetric/chsrc/releases/download/pre/"
 
-foreach ($name in $names) {
-  curl -LO "${url_prefix}${name}"
-}
+Write-Output "=> Starting downloads..."
+
+$names | ForEach-Object -Parallel {
+  $name = $_
+  $url = $using:url_prefix + $name
+  $dest = $using:destination
+
+  Write-Output "  - Downloading $name"
+  # -s 阻止输出，避免并行输出混乱
+  curl -s -LO $url --output-dir $dest
+  Write-Output "  √ Completed $name"
+} -ThrottleLimit 5  # 限制同时下载5个文件
+
+Write-Output "=> All downloads completed!"
 
 Set-Location -
 Write-Output "=> Downloaded to $destination"
