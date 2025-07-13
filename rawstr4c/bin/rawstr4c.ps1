88558af591e21c70e789699a19e698a52295d57d
Aoran Zeng <ccmywish@qq.com>
2025-07-13 19:16:52 +0800

    Move to `bin`

--- rawstr4c/bin/rawstr4c.ps1
+++ rawstr4c/bin/rawstr4c.ps1
@@ -1,19 +1,19 @@
 # ---------------------------------------------------------------
-# File Name     : run.ps1
+# File Name     : rawstr4c.ps1
 # File Authors  : Aoran Zeng <ccmywish@qq.com>
 # Contributors  :  Nul None  <nul@none.org>
 # Created On    : <2025-07-12>
 # Last Modified : <2025-07-13>
 #
-# Run rawstr4c script
+# Run rawstr4c.raku script
 #
 # Usage:
 #
 #   用户必须在代码根目录中运行此文件
 #
-#     .\rawstr4c\run.ps1 [--debug] <Markdown.md>
+#     .\rawstr4c\bin\rawstr4c.ps1 [--debug] <Markdown.md>
 #
-#     .\rawstr4c\run.ps1 [--debug] <Dir>
+#     .\rawstr4c\bin\rawstr4c.ps1 [--debug] <Dir>
 # ---------------------------------------------------------------
 
 # -I 选项必须在文件前面
