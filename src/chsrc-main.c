f9ad8ea242beed06f421ed7aed75873bc60ad22b
Aoran Zeng <ccmywish@qq.com>
2025-07-19 05:41:06 +0800

    Prerelease `v0.2.2-alpha3`

--- src/chsrc-main.c
+++ src/chsrc-main.c
@@ -31,13 +31,13 @@
  *                 |   Rui Yang     <techoc@foxmail.com>
  *                 |
  * Created On      : <2023-08-28>
- * Last Modified   : <2025-07-16>
+ * Last Modified   : <2025-07-19>
  *
  * chsrc: Change Source —— 全平台通用命令行换源工具
  * ------------------------------------------------------------*/
 
-#define Chsrc_Version        "0.2.2-alpha2"
-#define Chsrc_Release_Date   "2025/07/16"
+#define Chsrc_Version        "0.2.2-alpha3"
+#define Chsrc_Release_Date   "2025/07/19"
 #define Chsrc_Maintain_URL   "https://github.com/RubyMetric/chsrc"
 #define Chsrc_Maintain_URL2  "https://gitee.com/RubyMetric/chsrc"
 
