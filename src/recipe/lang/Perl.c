a08ef58579203d7fce4ec03c059852f385a02d51
Aoran Zeng <ccmywish@qq.com>
2025-08-21 18:47:33 +0800

    添加维护者信息

--- src/recipe/lang/Perl.c
+++ src/recipe/lang/Perl.c
@@ -12,12 +12,11 @@ pl_perl_prelude ()
 
   chef_set_created_on   (this, "2023-09-31");
   chef_set_last_updated (this, "2025-08-10");
-  chef_set_sources_last_updated (this, "2024-05-24");
+  chef_set_sources_last_updated (this, "2025-08-21");
 
   chef_set_chef (this, NULL);
   chef_set_cooks (this, 1, "@ccmywish");
-  chef_set_contributors (this, 0);
-  // TODO: 添加 @hezonglun
+  chef_set_contributors (this, 1, "@hezonglun");
 
   chef_allow_local_mode (this, CanNot, NULL, NULL);
   chef_forbid_english(this);
