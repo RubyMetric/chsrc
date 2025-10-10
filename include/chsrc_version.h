b1efba41f529d1fa8d10dc5ac9d4e4fa836082ba
Mikachu2333 <mikachu.23333@zohomail.com>
2025-10-10 23:34:18 +0800

    feat(build): 支持编译资源文件 修复just 修复makefile

---
+++ include/chsrc_version.h
@@ -0,0 +1,24 @@
+/** ------------------------------------------------------------
+ * Copyright © 2023-2025 曾奥然, 郭恒
+ * SPDX-License-Identifier: MIT
+ * -------------------------------------------------------------
+ * Lib Authors   :   曾奥然     <ccmywish@qq.com>
+ * Contributors  : Mikachu2333  <mikachu.23333@zohomail.com>
+ *               |
+ * Created On    : <2025-10-10>
+ * Last Modified : <2025-10-10>
+
+ * ！！！警告！！！
+ * 发布前请修改此处的内容
+ * ------------------------------------------------------------*/
+
+#define Chsrc_Version        "0.2.3"
+#define Chsrc_Release_Date   "2025/10/06"
+
+// 以下宏仅用于 Windows
+#if defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__)
+  #define CHSRC_VERSION_MAJOR  0
+  #define CHSRC_VERSION_MINOR  2
+  #define CHSRC_VERSION_PATCH  3
+  #define CHSRC_VERSION_STRING "0.2.3"
+#endif
