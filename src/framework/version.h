0bcc4c22394fbd7e7fcafe8b1738dce18100f2d8
Aoran Zeng <ccmywish@qq.com>
2026-09-07 18:58:20 +0800

    Prerelease `v0.2.7.2`

--- src/framework/version.h
+++ src/framework/version.h
@@ -8,11 +8,11 @@
  * 发布新版本前请修改此文件
  * ------------------------------------------------------------*/
 
-#define Chsrc_Version        "0.2.7.1"
+#define Chsrc_Version        "0.2.7.2"
 // 以下四个宏仅用于 resource/chsrc.rc
 #define Chsrc_Version_Major  0
 #define Chsrc_Version_Minor  2
 #define Chsrc_Version_Patch  7
-#define Chsrc_Version_Pre    1
+#define Chsrc_Version_Pre    2
 
-#define Chsrc_Release_Date   "2026/09/05"
+#define Chsrc_Release_Date   "2026/09/07"
